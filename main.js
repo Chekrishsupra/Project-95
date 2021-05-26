c84d54dc0134bddcca281fbf3854af688ca3bce3
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-05-26 13:35:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function login() {
+    Name = document.getElementById("Name").value;
+    Password = document.getElementById("password").value;
+    localStorage.setItem("Name" , Name);
+    localStorage.setItem("Password" , Password);
+    window.location = "chat.html";
+}
